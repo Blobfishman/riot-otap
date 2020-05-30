3cc6c7989cee8bb5d024e555bea7b0d5c4d6e755
Eric <erneumann@uni-osnabrueck.de>
2020-05-30 16:11:59 +0200

    added server

---
+++ Server/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "shell.h"
+#include "thread.h"
+
+static char server_stack_note[THREAD_STACKSIZE_DEFAULT];
+static char server_stack_pc[THREAD_STACKSIZE_DEFAULT];
+
+extern void *receive_data(void *arg);
+extern void *receive_update(void *arg);
+
+static const shell_command_t commands[] = {
+    { NULL, NULL, NULL }
+};
+
+int main(void) {
+
+    thread_create(server_stack_note, sizeof(server_stack_note),
+                    THREAD_PRIORITY_MAIN - 1,
+                    THREAD_CREATE_STACKTEST,
+                    receive_data,
+                    NULL, "receive_data");
+
+    thread_create(server_stack_pc, sizeof(server_stack_pc),
+                    THREAD_PRIORITY_MAIN - 1,
+                    THREAD_CREATE_STACKTEST,
+                    receive_update,
+                    NULL, "receive_update");
+
+    
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
